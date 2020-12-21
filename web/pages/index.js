49d305ae566350a423346f0b92034e09b52e99e9
Junho Yeo <hanaro0704@gmail.com>
2020-12-21 21:33:08 +0900

    implemented project-card & list

--- web/pages/index.js
+++ web/pages/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import { styled } from "@linaria/react";
 
 import DashboardAppbar from "../components/appbar/dashboard.appbar";
+import ProjectCard from "../components/home/project-card";
 import Button from "../components/button";
+
+const exampleProjects = Array(6).fill({
+    title: "My New Project",
+    lastEdit: "Updated A Day ago",
+    preview: "/assets/examples/project.png",
+});
 
 const Home = () => {
     return (
@@ -14,6 +21,11 @@ const Home = () => {
                         <CreateButton>Create New</CreateButton>
                     </BannerContainer>
                 </BannerWrapper>
+                <ProjectList>
+                    {exampleProjects.map((project, projectIndex) => (
+                        <ProjectCard key={projectIndex} {...project} />
+                    ))}
+                </ProjectList>
             </Wrapper>
         </>
     );
@@ -25,6 +37,9 @@ const Wrapper = styled.main`
     flex: 1;
     padding-top: 55px;
     padding-bottom: 55px;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
 `;
 
 const BannerWrapper = styled.div`
@@ -44,4 +59,17 @@ const CreateButton = styled(Button)`
     position: absolute;
     bottom: -26px;
     right: 0;
+`;
+
+const ProjectList = styled.ul`
+    padding: 0;
+    list-style-type: none;
+    width: 92%;
+    margin: 0 auto;
+    flex: 1;
+    padding-top: 80px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(425px, 1fr));
+    grid-column-gap: 1.5rem;
+    grid-row-gap: 2.5rem;
 `;
