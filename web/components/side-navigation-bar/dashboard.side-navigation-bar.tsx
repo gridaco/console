f0b3bf2a24563eb553a230943b14414b1943e59b
Junho Yeo <hanaro0704@gmail.com>
2020-12-28 07:16:35 +0900

    refactor(components): detach constant dashboard navigations

--- web/components/side-navigation-bar/dashboard.side-navigation-bar.tsx
+++ web/components/side-navigation-bar/dashboard.side-navigation-bar.tsx
@@ -1,41 +1,9 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { styled } from '@linaria/react';
 
-import ListItem, { INavigation } from './dashboard.side-item';
-import { useRouter } from 'next/router';
-
-const navigations: INavigation[] = [
-  {
-    name: 'Overview',
-    // path: '/overview',
-    icon: '/assets/icons/mdi_remove_red_eye.svg',
-  },
-  {
-    name: 'Screens',
-    path: '/screens',
-    icon: '/assets/icons/mdi_remove_red_eye.svg',
-  },
-  {
-    name: 'Fonts',
-    // path: '/fonts',
-    icon: '',
-  },
-  {
-    name: 'Icons',
-    // path: '/icons',
-    icon: '',
-  },
-  {
-    name: 'Assets',
-    path: '/assets',
-    icon: '/assets/icons/mdi_web_asset.svg',
-  },
-  {
-    name: 'Code',
-    // path: '/code',
-    icon: '/assets/icons/icons.svg',
-  },
-];
+import ListItem from './dashboard.side-item';
+import { navigations } from './dashboard.constants';
 
 export function DashboardSideNavigationBar() {
   const router = useRouter();
