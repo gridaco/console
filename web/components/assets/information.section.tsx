cfb6581a992cdc416589ed13f365fcd118c7e293
Junho Yeo <hanaro0704@gmail.com>
2020-12-28 06:34:07 +0900

    feat(pages/assets): select & sections for SelectedAssetInformation

---
+++ web/components/assets/information.section.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { styled } from '@linaria/react';
+
+interface IInformationSection {
+  title: string;
+  icon?: React.ReactNode;
+}
+
+const InformationSection: React.FC<IInformationSection> = ({
+  title,
+  icon,
+  children,
+}) => {
+  return (
+    <Section>
+      <Header>
+        <Title>{title}</Title>
+        {icon}
+      </Header>
+      {children}
+    </Section>
+  );
+};
+
+export default InformationSection;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding-top: 18px;
+  padding-bottom: 16px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const Title = styled.h2`
+  margin: 0;
+  font-size: 18px;
+  line-height: 1.2;
+  color: #000000;
+`;
--- web/components/assets/selected-asset-information.tsx
+++ web/components/assets/selected-asset-information.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
 import { styled } from '@linaria/react';
 
+import Select from './select';
+import InformationSection from './information.section';
+import IconButton from '../icon-button';
+
+const exampleManagers = Array(4).fill({
+  profile: '/assets/examples/profile.png',
+});
+
 const SelectedAssetInformation: React.FC = () => {
   return (
     <Container>
       <Title>Lint Cover Illust</Title>
       <FilePath>illusts/lint/eye-cover-main</FilePath>
+      <SelectWrapper>
+        <Select>
+          <option selected>English(Default)</option>
+          <option>한국어</option>
+        </Select>
+      </SelectWrapper>
       <PreviewImage src={'/assets/examples/project.png'} />
       <UpdatedAt>last updated 2 hours ago by @gy</UpdatedAt>
+      <SectionList>
+        <InformationSection
+          title="Manager"
+          icon={
+            <IconButton>
+              <IconImage src="/assets/icons/mdi_add_circle_outline_round.svg" />
+            </IconButton>
+          }
+        >
+          <ManagerList>
+            {exampleManagers.map(({ profile }, managerIndex) => (
+              <li key={managerIndex}>
+                <ManagerImage src={profile} />
+              </li>
+            ))}
+          </ManagerList>
+        </InformationSection>
+        <InformationSection title="Variants" />
+        <InformationSection title="Locales" />
+        <InformationSection title="Used in" />
+      </SectionList>
     </Container>
   );
 };
@@ -39,6 +74,13 @@ const FilePath = styled.p`
   color: #636363;
 `;
 
+const SelectWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin-top: 28px;
+`;
+
 const PreviewImage = styled.img`
   object-fit: cover;
   height: 332px;
@@ -52,4 +94,28 @@ const UpdatedAt = styled.p`
   font-size: 16px;
   line-height: 1.2;
   color: #636363;
+`;
+
+const SectionList = styled.div`
+  margin-top: 18px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const IconImage = styled.img``;
+
+const ManagerList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ManagerImage = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 11px;
 `;
