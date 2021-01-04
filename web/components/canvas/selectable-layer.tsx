4ff3a3941cfdaf88db37453dd6f9e707c009c4b9
Junho Yeo <hanaro0704@gmail.com>
2021-01-04 15:23:14 +0900

    refactor(components): prop structuring & proper types

--- web/components/canvas/selectable-layer.tsx
+++ web/components/canvas/selectable-layer.tsx
@@ -1,60 +1,53 @@
 import React, { useState } from 'react';
 import { Group, Rect } from 'react-konva';
 
-export function SelectableLayer(props: {
+interface ISelectableLayer {
   children: JSX.Element;
   width: number;
   height: number;
   selected?: boolean;
   onFocusChange?: (id: string, focus: boolean) => void;
-}) {
+}
+
+export const SelectableLayer: React.FC<ISelectableLayer> = ({
+  children,
+  selected,
+  onFocusChange,
+  ...size
+}) => {
   const [hover, setHover] = useState<boolean>();
 
-  const childId = props.children.key as string;
+  const childId = (children.key || '').toString();
 
-  const handleClick = (e: any) => {
-    if (props.onFocusChange) {
-      props.onFocusChange(childId as string, true);
+  const handleClick = () => {
+    if (onFocusChange) {
+      onFocusChange(childId, true);
     }
   };
 
-  const handleDoubleClick = (e: any) => {
-    if (props.onFocusChange) {
-      props.onFocusChange(childId, true);
-    }
-  };
+  // const handleDoubleClick = () => {
+  //   if (onFocusChange) {
+  //     onFocusChange(childId, true);
+  //   }
+  // };
 
-  const handleOutFocus = (e: any) => {
-    setHover(false);
-  };
-  const handleInFocus = (e: any) => {
-    setHover(true);
-  };
+  const handleOutFocus = () => setHover(false);
+
+  const handleInFocus = () => setHover(true);
 
   return (
     <Group
-      width={props.width}
-      height={props.height}
       onMouseLeave={handleOutFocus}
       onMouseEnter={handleInFocus}
-      // ondblclick={handleDoubleClick}
+      // onDoubleClick={handleDoubleClick}
       onClick={handleClick}
+      {...size}
     >
       {/* selection indicator */}
-      <Rect
-        width={props.width}
-        height={props.height}
-        stroke={'#FF2B5E'}
-        strokeWidth={props.selected ? 2 : 0}
-      />
+      <Rect stroke="#FF2B5E" strokeWidth={selected ? 2 : 0} {...size} />
       {/* hover indicator */}
-      <Rect
-        width={props.width}
-        height={props.height}
-        stroke={'#5900B2'}
-        strokeWidth={hover ? 1 : 0}
-      />
-      {props.children}
+      <Rect stroke="#5900B2" strokeWidth={hover ? 1 : 0} {...size} />
+      {children}
     </Group>
   );
-}
+};
