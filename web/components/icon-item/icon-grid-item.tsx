db6a874754e87f1bed595ff834b5164c91b76bf5
Junho Yeo <hanaro0704@gmail.com>
2021-01-05 10:24:17 +0900

    feat(pages/icons): create component IconGridItem

---
+++ web/components/icon-item/icon-grid-item.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { styled } from '@linaria/react';
+
+interface IIconGridItem {
+  icon: React.ReactNode;
+  name: string;
+}
+
+const IconGridItem: React.FC<IIconGridItem> = ({ icon, name }) => {
+  return (
+    <Container>
+      <IconWrapper>{icon}</IconWrapper>
+      <Name>{name}</Name>
+    </Container>
+  );
+};
+
+export default IconGridItem;
+
+const Container = styled.li`
+  display: flex;
+  flex-direction: column;
+`;
+
+const IconWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  background: #fafafa;
+  border-radius: 2px;
+`;
+
+const Name = styled.h3`
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 1.2;
+  letter-spacing: 0.3px;
+  color: #000000;
+  margin-top: 8px;
+`;
