a6265f05fc51260f3e227dd8b4876af9a9717089
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-20 02:25:19 +0900

    add eslint

--- web/pages/handoff/3d/index.tsx
+++ web/pages/handoff/3d/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { styled } from "linaria/react";
+// FIXME:
 // import ModelViewer_withnossr from "@app/handoff/handoff-3d/model-viewer";
 // const ModelViewer_withnossr = dynamic(
 //   () => import("@app/handoff/handoff-3d/model-viewer"),
