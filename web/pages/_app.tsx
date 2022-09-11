b00d8e8d84ab499cfd011159ff04be1b8ccc8322
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-12 06:56:50 +0900

    add ga

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,19 +1,39 @@
-import Modal from 'react-modal';
-import { RecoilRoot } from 'recoil';
-import QueryParamProvider from '../components/query-param-provider';
+import Head from "next/head";
+import Script from "next/script";
+import Modal from "react-modal";
+import { RecoilRoot } from "recoil";
+import QueryParamProvider from "../components/query-param-provider";
 
-import '../styles/globals.css';
+import "../styles/globals.css";
 // import 'monaco-editor/esm/vs/base/browser/ui/codicons/codicon/codicon.css';
 
-Modal.setAppElement('#__next');
+Modal.setAppElement("#__next");
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
-      <QueryParamProvider>
-        <Component {...pageProps} />
-      </QueryParamProvider>
-    </RecoilRoot>
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-2BP4168N58"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-2BP4168N58');
+        `}
+      </Script>
+      <Head>
+        <title>Grida console</title>
+      </Head>
+      <RecoilRoot>
+        <QueryParamProvider>
+          <Component {...pageProps} />
+        </QueryParamProvider>
+      </RecoilRoot>
+    </>
   );
 }
 
